996cd5e0d613277b64aff51ead7034f8028daa27
Ahmad <ahmad_kanj_22@hotmail.com>
2021-05-23 13:42:07 +0300

    First Commit

--- main.js
+++ main.js
@@ -29,4 +29,17 @@ ready(() => {
       document.querySelector(".errorMessage").style.display = "block";
     }
   });
+
+  window.addEventListener(
+    "keydown",
+    function (event) {
+      if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.querySelector(".submitButton").click();
+      }
+    },
+    true
+  );
 });
